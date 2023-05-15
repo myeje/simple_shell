a8f4efcdfb4adf1d385fcee78733aae59d095155
Joshua Emore <ehimareemore@gmail.com>
2023-05-15 09:07:22 -0700

    Simple shell

--- main.c
+++ main.c
@@ -1,17 +1,32 @@
 #include "main.h"
 
+/**
+ * main - simple shell
+ * @argc: Number of arguments in argv
+ * @argv: Arguments to pass to the program
+ * Return: 0 if successful
+ */
 int main(int argc, char **argv)
 {
-	char *prompt = "SimpleShell$ ";
+	char *prompt = "QueenShell$ ";
 	char *readline;
+	ssize_t read_cmd;
 	size_t i = 0;
 
-	(void)argc; (void)argv;
-	
-	write(STDOUT_FILENO, prompt, 13);
-	getline(&readline, &i, stdin);
-	write(STDOUT_FILENO, readline, i);
+	(void)argc;
+	(void)argv;
 
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 13);
+		read_cmd = getline(&readline, &i, stdin);
+		if (read_cmd == -1)
+		{
+			write(STDOUT_FILENO, "\nQuitting Shell...\n", 17);
+			return (-1);
+		}
+		write(STDOUT_FILENO, readline, i);
+	}
 	free(readline);
 	return (0);
 }
