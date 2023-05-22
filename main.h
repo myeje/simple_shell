df8c781606c7ea21e390235e3fdca154317ef093
queenoko <quimaudongwo@gmail.com>
2023-05-22 05:42:22 -0700

    getline

--- main.h
+++ main.h
@@ -15,4 +15,12 @@ void exec(char **argv);
 char *path_finder(char *command);
 void shell_env(void);
 extern char **environ;
+void *_expan_mem(void *ptr, unsigned int prev_size, unsigned int add_size);
+void give_linptr(char **lineptr, size_t *n, char *buff, size_t q);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+
+
+
+
+
 #endif
